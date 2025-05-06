c39246a98fef44b248cc3ed2a2dd1a1e6981f4eb
Hanabusa Masahiro <masa@hanabusa.net>
2025-05-07 01:44:36 +0900

    feat: Use encrypted session Cookie in example

--- build_lambda.sh
+++ build_lambda.sh
@@ -7,7 +7,7 @@ podman run -it --rm \
   -v ~/.cargo/registry:/usr/local/cargo/registry:z \
   -v .:/build:z \
   musl_builder \
-  cargo build --release --target=x86_64-unknown-linux-musl --example=sign-in-with-google \
+  cargo build --release --target=x86_64-unknown-linux-musl --example=sign-in-with-google --features=session-store \
 && \
 7za a  lambda_function.zip \
        target/x86_64-unknown-linux-musl/release/examples/sign-in-with-google && \
--- examples/sign-in-with-google.rs
+++ examples/sign-in-with-google.rs
@@ -6,12 +6,16 @@
 use axum::extract::{Query, State};
 use axum::http::{StatusCode, header::HeaderMap};
 use axum::response::Html;
-use axum_extra::TypedHeader;
 type GoogleClient = tiny_oidc_rp::Client<tiny_oidc_rp::GoogleProvider>;
+
+const SESSION_COOKIE_NAME: &str = "__Http-session";
+const SESSION_PAYLOAD_OIDC: u8 = 0;
+const SESSION_PAYLOAD_USER: u8 = 1;
 
 struct AppState {
     oidc_client: GoogleClient,
     session_store: InMemoryLoginSessionStore,
+    cookie_key: tiny_oidc_rp::session_store::SessionStoreKey,
 }
 
 #[tokio::main]
@@ -30,15 +34,24 @@ async fn main() -> Result<(), lambda_http::Error> {
         .build()
         .unwrap();
 
+    // Cookie key
+    let cookie_key = tiny_oidc_rp::session_store::SessionStoreKey::new(
+        0,
+        &std::env::var("COOKIE_SECRET").unwrap(),
+    )
+    .unwrap();
+
     let app_state = std::sync::Arc::new(AppState {
         oidc_client,
         session_store: InMemoryLoginSessionStore::new(),
+        cookie_key,
     });
 
     // build routes
     let app = Router::new()
         .route("/login", get(oidc_start_auth))
         .route("/login/callback", get(oidc_return_from_idp))
+        .route("/login/result", get(oidc_result))
         .route("/", get(root))
         .with_state(app_state);
 
@@ -97,13 +110,18 @@ async fn oidc_start_auth(
         header::HeaderValue::from_str(&auth_url.to_string()).unwrap(),
     );
     // Login session key in Cookie
+    let cookie = state
+        .cookie_key
+        .encrypt(SESSION_COOKIE_NAME, &session.key(), SESSION_PAYLOAD_OIDC)
+        .unwrap()
+        .http_only(true)
+        .path("/")
+        .same_site(cookie::SameSite::Lax)
+        .secure(true)
+        .build();
     headers.insert(
         header::SET_COOKIE,
-        header::HeaderValue::from_str(&format!(
-            "__Host-LoginSesion={}; path=/; Secure; HttpOnly; SameSite=Lax;",
-            session.key()
-        ))
-        .unwrap(),
+        header::HeaderValue::from_str(&cookie.to_string()).unwrap(),
     );
     Ok((StatusCode::SEE_OTHER, headers, ""))
 }
@@ -115,55 +133,99 @@ struct OidcAuthResult {
 }
 /// Redirect from IdP with sessin and code
 async fn oidc_return_from_idp(
-    TypedHeader(cookie): TypedHeader<axum_extra::headers::Cookie>,
+    session_cookie: SessionCookie,
     State(state): State<std::sync::Arc<AppState>>,
     Query(auth_result): Query<OidcAuthResult>,
-) -> axum::response::Result<(HeaderMap, Html<String>)> {
+) -> axum::response::Result<(StatusCode, HeaderMap, &'static str)> {
+    use axum::http::StatusCode;
     use axum::http::header;
+    use axum::response::ErrorResponse;
 
     // Login session key from cookie
-    let session_key = cookie.get("__Host-LoginSesion").unwrap();
+    let session_key = if let SessionCookie::OidcSessionKey(session_key) = session_cookie {
+        session_key
+    } else {
+        log::error!("No OIDC session key in cookie");
+        return Err(ErrorResponse::from(StatusCode::BAD_REQUEST));
+    };
 
     // Load login session state
     // Removing from session store is important to prevent replay attacks
-    let session = state.session_store.get_remove(session_key).await.unwrap();
+    let session = state
+        .session_store
+        .get_remove(&session_key)
+        .await
+        .ok_or(ErrorResponse::from(StatusCode::BAD_REQUEST))?;
 
     // Get ID token from token endpoint
     let idtoken = state
         .oidc_client
         .authenticate::<GoogleIdTokenExtraClaims>(&auth_result.state, &auth_result.code, &session)
         .await
-        .unwrap();
+        .map_err(|e| {
+            log::error!("Cookie decode error {:?}", e);
+            ErrorResponse::from(StatusCode::BAD_REQUEST)
+        })?;
     // If you don't need extra claims, call as authenticate::<()>(...)
 
+    // Logged in session cookie
+    let user = User {
+        sub: idtoken.subject().to_string(),
+        name: idtoken.name().unwrap_or_default().to_string(),
+        email: idtoken.email().unwrap_or_default().to_string(),
+        picture_url: idtoken
+            .extra()
+            .picture
+            .as_deref()
+            .unwrap_or_default()
+            .to_string(),
+    };
+    let user_cookie = state
+        .cookie_key
+        .encrypt(SESSION_COOKIE_NAME, &user, SESSION_PAYLOAD_USER)
+        .unwrap()
+        .http_only(true)
+        .path("/")
+        .same_site(cookie::SameSite::Lax)
+        .secure(true)
+        .build();
+
+    // Redirect to /login/result page with User session cookie
     let mut headers = HeaderMap::new();
-    // Clear OIDC session cookie
+    headers.insert(
+        header::LOCATION,
+        header::HeaderValue::from_static("/login/result"),
+    );
     headers.insert(
         header::SET_COOKIE,
-        header::HeaderValue::from_static(
-            "__Host-LoginSesion=; path=/; Secure; HttpOnly; SameSite=Lax; Max-Age=0;",
-        ),
+        header::HeaderValue::from_str(&user_cookie.to_string()).unwrap(),
     );
-    // Session cookie
-    headers.insert(
-        header::SET_COOKIE,
-        header::HeaderValue::from_static(
-            "__Host-sesion=loggedin; path=/; Secure; HttpOnly; SameSite=Lax;",
-        ),
-    );
+
+    Ok((StatusCode::SEE_OTHER, headers, ""))
+}
+
+/// Print OIDC login result
+async fn oidc_result(session_cookie: SessionCookie) -> axum::response::Result<Html<String>> {
+    use axum::http::StatusCode;
+    use axum::response::ErrorResponse;
+
+    // Extract user infomation from cookie
+    let user = if let SessionCookie::UserSession(user) = session_cookie {
+        user
+    } else {
+        log::error!("No user information in cookie");
+        return Err(ErrorResponse::from(StatusCode::BAD_REQUEST));
+    };
 
     // <img> for picture url
-    let img = idtoken
-        .extra()
-        .picture
-        .as_ref()
-        .map(|pic_url| {
-            // !!! CAUTION !!! You must escape string for production
-            format!("<img src={} alt=\"[Your avatar]\" />", pic_url)
-        })
-        .unwrap_or_default();
+    let img = if user.picture_url.is_empty() {
+        "".to_string()
+    } else {
+        // !!! CAUTION !!! You must escape string for production
+        format!("<img src={} alt=\"[Your avatar]\" />", user.picture_url)
+    };
 
-    // Debug print body
+    // Make body
     let body = format!(
         r###"<!DOCTYPE html>
 <html>
@@ -184,19 +246,70 @@ async fn oidc_return_from_idp(
 </html>
 "###,
         // !!! CAUTION !!! You must escape string for production
-        idtoken.subject(),
-        idtoken.name().unwrap_or("-"),
-        idtoken.email().unwrap_or("-"),
+        user.sub,
+        user.name,
+        user.email,
         img
     );
 
-    Ok((headers, Html(body)))
+    Ok(Html(body))
 }
 
 /// Extra ID token claims
 #[derive(serde::Deserialize)]
 struct GoogleIdTokenExtraClaims {
     picture: Option<String>,
+}
+
+/// Session cookie
+enum SessionCookie {
+    OidcSessionKey(String),
+    UserSession(User),
+}
+
+/// User information in Cookie
+#[derive(serde::Deserialize, serde::Serialize)]
+struct User {
+    sub: String,
+    name: String,
+    email: String,
+    picture_url: String,
+}
+
+impl axum::extract::FromRequestParts<std::sync::Arc<AppState>> for SessionCookie {
+    type Rejection = axum::http::StatusCode;
+
+    // Extract SessionCookie from axum Request
+    async fn from_request_parts(
+        parts: &mut http::request::Parts,
+        state: &std::sync::Arc<AppState>,
+    ) -> Result<Self, Self::Rejection> {
+        use axum::http::StatusCode;
+        let cookies = axum_extra::extract::CookieJar::from_request_parts(parts, state)
+            .await
+            .map_err(|_e| StatusCode::BAD_REQUEST)?;
+        if let Some(session_cookie) = cookies.get(SESSION_COOKIE_NAME) {
+            match tiny_oidc_rp::session_store::SessionStoreKey::payload_ver(&session_cookie) {
+                Some(SESSION_PAYLOAD_OIDC) => {
+                    let session_key = state
+                        .cookie_key
+                        .decrypt::<String>(&session_cookie)
+                        .map_err(|_| StatusCode::BAD_REQUEST)?;
+                    Ok(Self::OidcSessionKey(session_key))
+                }
+                Some(SESSION_PAYLOAD_USER) => {
+                    let user = state
+                        .cookie_key
+                        .decrypt::<User>(&session_cookie)
+                        .map_err(|_| StatusCode::BAD_REQUEST)?;
+                    Ok(Self::UserSession(user))
+                }
+                _ => Err(StatusCode::BAD_REQUEST),
+            }
+        } else {
+            Err(StatusCode::BAD_REQUEST)
+        }
+    }
 }
 
 /// This is example only, HashMap based in-memory session state store
